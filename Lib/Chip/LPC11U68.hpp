82b0f5a25fca4983dc15e1bdf7a24dd5b066974d
bjoekeldude <bjoekeldude@gmail.com>
2015-07-29 12:36:17 +0200

    updated chipfile structure

--- Lib/Chip/LPC11U68.hpp
+++ Lib/Chip/LPC11U68.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
-#include "NXP/LPC11/M0+/U6xE6x/StartUp.hpp"
-#include "NXP/LPC11/M0+/U6xE6x/Adc.hpp"
-#include "NXP/LPC11/M0+/U6xE6x/Interrupt.hpp"
-#include "NXP/LPC11/M0+/U6xE6x/Io.hpp"
-#include "NXP/LPC11/M0+/U6xE6x/Timer.hpp"
-#include "NXP/LPC11/M0+/U6xE6x/Uart.hpp"
-#include "NXP/LPC11/M0+/U6xE6x/Usb.hpp"
+#include "ARM Cortex M0+/NXP/LPC11U6xE6x/StartUp.hpp"
+#include "ARM Cortex M0+/NXP/LPC11U6xE6x/Adc.hpp"
+#include "ARM Cortex M0+/NXP/LPC11U6xE6x/Interrupt.hpp"
+#include "ARM Cortex M0+/NXP/LPC11U6xE6x/Io.hpp"
+#include "ARM Cortex M0+/NXP/LPC11U6xE6x/Timer.hpp"
+#include "ARM Cortex M0+/NXP/LPC11U6xE6x/Uart.hpp"
+#include "ARM Cortex M0+/NXP/LPC11U6xE6x/Usb.hpp"
