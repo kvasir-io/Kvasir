901755f56ec5665387425b44c95ef85149431e6a
Odin Holmes <holmes.odin@gmail.com>
2016-03-18 15:16:31 +0100

    added usb unit tests

--- test/usb.cpp
+++ test/usb.cpp
@@ -12,16 +12,22 @@ using PacketT = Kvasir::Usb::Packet <Kvasir::Usb::CompactPacket::ImplType>;
 using Alloc = Kvasir::Usb::CompactPacket::Allocator<PacketT,12>;
 using Device = Kvasir::Usb::Device<Alloc, Kvasir::Usb::CompactPacket::Queue<PacketT>, Kvasir::Usb::CompactPacket::Transfer<PacketT, Alloc>>;
 
+template<typename T>
+decltype(Alloc::allocate()) makePacket(std::initializer_list<T> l) {
+	auto packet = Alloc::allocate();
+	for (auto d : l) {
+		packet.pushBack(d);
+	}
+	return packet;
+}
+
+
 int usbTest()
 {
 	Alloc::initialize();
 	{
-		auto packet = Alloc::allocate();
 		//set address
-		for (auto d : { 0, 5, 5, 0, 0, 0, 0, 0 }) {
-			packet.pushBack(d);
-		}
-		auto cmd = Device::onSetupPacket(std::move(packet));
+		auto cmd = Device::onSetupPacket(makePacket({ 0, 5, 5, 0, 0, 0, 0, 0 }));
 		if (cmd.type_ != Device::HalCommand::Type::setAddress) {
 			return 1;
 		}
@@ -38,11 +44,7 @@ int usbTest()
 	}
 	{
 		//GetDevice descriptor
-		auto packet = Alloc::allocate();
-		for (auto d : { 0x80, 6, 0, 1, 0, 0, 8, 0 }) {
-			packet.pushBack(d);
-		}
-		auto cmd = Device::onSetupPacket(std::move(packet));
+		auto cmd = Device::onSetupPacket(makePacket({ 0x80, 6, 0, 1, 0, 0, 8, 0 }));
 		if (cmd.type_ != Device::HalCommand::Type::noAction) {
 			return 1;
 		}
@@ -59,11 +61,7 @@ int usbTest()
 	}
 	{
 		//GetDevice descriptor
-		auto packet = Alloc::allocate();
-		for (auto d : { 0x80, 0x06, 0x00, 0x01, 0x00, 0x00, 0x12, 0x00 }) {
-			packet.pushBack(d);
-		}
-		auto cmd = Device::onSetupPacket(std::move(packet));
+		auto cmd = Device::onSetupPacket(makePacket({ 0x80, 0x06, 0x00, 0x01, 0x00, 0x00, 0x12, 0x00 }));
 		if (cmd.type_ != Device::HalCommand::Type::noAction) {
 			return 1;
 		}
@@ -80,11 +78,7 @@ int usbTest()
 	}
 	{
 		//Get Configuration descriptor
-		auto packet = Alloc::allocate();
-		for (auto d : { 0x80, 0x06, 0x00, 0x02, 0x00, 0x00, 0x09, 0x00 }) {
-			packet.pushBack(d);
-		}
-		auto cmd = Device::onSetupPacket(std::move(packet));
+		auto cmd = Device::onSetupPacket(makePacket({ 0x80, 0x06, 0x00, 0x02, 0x00, 0x00, 0x09, 0x00 }));
 		if (cmd.type_ != Device::HalCommand::Type::noAction) {
 			return 1;
 		}
@@ -101,11 +95,7 @@ int usbTest()
 	}
 	{
 		//Get Configuration descriptor
-		auto packet = Alloc::allocate();
-		for (auto d : { 0x80, 0x06, 0x00, 0x02, 0x00, 0x00, 0x4B, 0x00 }) {
-			packet.pushBack(d);
-		}
-		auto cmd = Device::onSetupPacket(std::move(packet));
+		auto cmd = Device::onSetupPacket(makePacket({ 0x80, 0x06, 0x00, 0x02, 0x00, 0x00, 0x4B, 0x00 }));
 		if (cmd.type_ != Device::HalCommand::Type::noAction) {
 			return 1;
 		}
@@ -122,7 +112,114 @@ int usbTest()
 		if (cmd2.packet_.getSize() != 11) {
 			return 1;
 		}
-		if (cmd2.packet_[2] != 2) {
+		if (cmd2.packet_[0] != 2) {
+			return 1;
+		}
+		auto cmd3 = Device::onControlIn(std::move(cmd2.packet_));
+		if (cmd3.type_ != Device::HalCommand::Type::noAction) {
+			return 1;
+		}
+		if (cmd3.packet_) {
+			return 1;
+		}
+	}
+	{
+		//Get String descriptor
+		auto cmd = Device::onSetupPacket(makePacket({ 0x80, 0x06, 0x00, 0x03, 0x00, 0x00, 0xFF, 0x00 }));
+		if (cmd.type_ != Device::HalCommand::Type::noAction) {
+			return 1;
+		}
+		if (cmd.packet_.getSize() != 4) {
+			return 1;
+		}
+		if (cmd.packet_[0] != 4) {
+			return 1;
+		}
+		auto cmd2 = Device::onControlIn(std::move(cmd.packet_));
+		if (cmd2.type_ != Device::HalCommand::Type::noAction) {
+			return 1;
+		}
+		if (cmd2.packet_) {
+			return 1;
+		}
+	}
+	{
+		//Get String descriptor
+		auto cmd = Device::onSetupPacket(makePacket({ 0x80, 0x06, 0x02, 0x03, 0x09, 0x04, 0xFF, 0x00 }));
+		if (cmd.type_ != Device::HalCommand::Type::noAction) {
+			return 1;
+		}
+		if (cmd.packet_.getSize() != 22) {
+			return 1;
+		}
+		if (cmd.packet_[0] != 0x16) {
+			return 1;
+		}
+		auto cmd2 = Device::onControlIn(std::move(cmd.packet_));
+		if (cmd2.type_ != Device::HalCommand::Type::noAction) {
+			return 1;
+		}
+		if (cmd2.packet_) {
+			return 1;
+		}
+	}
+	{
+		//Get String descriptor
+		auto cmd = Device::onSetupPacket(makePacket({ 0x80, 0x06, 0x03, 0x03, 0x09, 0x04, 0xFF, 0x00 }));
+		if (cmd.type_ != Device::HalCommand::Type::noAction) {
+			return 1;
+		}
+		if (cmd.packet_.getSize() != 22) {
+			return 1;
+		}
+		if (cmd.packet_[0] != 0x16) {
+			return 1;
+		}
+		auto cmd2 = Device::onControlIn(std::move(cmd.packet_));
+		if (cmd2.type_ != Device::HalCommand::Type::noAction) {
+			return 1;
+		}
+		if (cmd2.packet_) {
+			return 1;
+		}
+	}
+	{
+		//GetDevice descriptor
+		auto cmd = Device::onSetupPacket(makePacket({ 0x80, 0x06, 0x00, 0x01, 0x00, 0x00, 0x12, 0x00 }));
+		if (cmd.type_ != Device::HalCommand::Type::noAction) {
+			return 1;
+		}
+		if (cmd.packet_.getSize() != 18) {
+			return 1;
+		}
+		auto cmd2 = Device::onControlIn(std::move(cmd.packet_));
+		if (cmd2.type_ != Device::HalCommand::Type::noAction) {
+			return 1;
+		}
+		if (cmd2.packet_) {
+			return 1;
+		}
+	}
+	{
+		//Get Configuration descriptor
+		auto cmd = Device::onSetupPacket(makePacket({ 0x80, 0x06, 0x00, 0x02, 0x00, 0x00, 0x09, 0x01 }));
+		if (cmd.type_ != Device::HalCommand::Type::noAction) {
+			return 1;
+		}
+		if (cmd.packet_.getSize() != 64) {
+			return 1;
+		}
+		if (cmd.packet_[0] != 9) {
+			return 1;
+		}
+		auto cmd2 = Device::onControlIn(std::move(cmd.packet_));
+		if (cmd2.type_ != Device::HalCommand::Type::noAction) {
+			return 1;
+		}
+		if (cmd2.packet_.getSize() != 11) {
+			return 1;
+		}
+		if (cmd2.packet_[0] != 2) {
 			return 1;
 		}
 		auto cmd3 = Device::onControlIn(std::move(cmd2.packet_));
