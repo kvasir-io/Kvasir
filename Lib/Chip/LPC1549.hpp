ca86c6edb538d38007b90b91d8b1784034e28382
porkybrain <holmes.odin@gmail.com>
2016-05-31 20:21:10 +0200

    removed outdated stuff

--- Lib/Chip/LPC1549.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "ARM Cortex M3/NXP/LPC15xx/StartUp.hpp"
-//#include "ARM Cortex M3/NXP/LPC15xx/Adc.hpp"
-#include "ARM Cortex M3/NXP/LPC15xx/Interrupt.hpp"
-#include "ARM Cortex M3/NXP/LPC15xx/Io.hpp"
-#include "ARM Cortex M3/NXP/LPC15xx/Timer.hpp"
-#include "ARM Cortex M3/NXP/LPC15xx/Uart.hpp"
-//#include "ARM Cortex M3/NXP/LPC15xx/Usb.hpp"
--- Lib/Chip/LPC1768.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "ARM Cortex M3/NXP/LPC175x6x/StartUp.hpp"
-#include "ARM Cortex M3/NXP/LPC175x6x/Interrupt.hpp"
-#include "ARM Cortex M3/NXP/LPC175x6x/Io.hpp"
-#include "ARM Cortex M3/NXP/LPC175x6x/Timer.hpp"
-#include "ARM Cortex M3/NXP/LPC175x6x/Usb.hpp"
