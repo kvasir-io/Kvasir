f0db6912da38594810155ddd89eaff6fd030400b
Carlos van Rooijen <info@carlosvanrooijen.nl>
2016-11-30 15:59:32 +0100

    Modified nordic nrf52 setInput When you set a pin as input the input buffer will now be connected. This means that the user doesn't have to do a manual register write everytime he wants to set an input pin.

--- Lib/Chip/CM4/Nordic/nrf52/Io.hpp
+++ Lib/Chip/CM4/Nordic/nrf52/Io.hpp
@@ -9,12 +9,14 @@ namespace Kvasir{
             Register::Action<Register::FieldLocation<Register::Address<0x5000050C,0xFFFFFFFF>,(1<<Pin)>,Register::WriteLiteralAction<(1<<Pin)>>{};
 
         template<int Pin>
-        struct MakeAction<Action::Input,Register::PinLocation<0,Pin>> :
-            Register::Action<Register::FieldLocation<Register::Address<0x5000051C,0x00000000>,(1<<Pin)>,Register::WriteLiteralAction<(0<<Pin)>>{};
-
+        struct MakeAction<Action::Input,Register::PinLocation<0,Pin>> {
+            using Type = brigand::list<Register::Action<Register::FieldLocation<Register::Address<0x5000051C,0xFFFFFFFF>,(1<<Pin)>,Register::WriteLiteralAction<(1<<Pin)>>, 
+		  //We add this extra action so that the input buffer will be connected when you set the pin as an input.
+		Register::Action<Register::FieldLocation<Register::Address<0x50000700+4*Pin,0x00000000>,(1<<1)>,Register::WriteLiteralAction<0>>>;
+	};
         template<int Pin>
         struct MakeAction<Action::Output,Register::PinLocation<0,Pin>> :
-            Register::Action<Register::FieldLocation<Register::Address<0x50000518,0x00000000>,(1<<Pin)>,Register::WriteLiteralAction<(1<<Pin)>>{};
+            Register::Action<Register::FieldLocation<Register::Address<0x50000518,0xFFFFFFFF>,(1<<Pin)>,Register::WriteLiteralAction<(1<<Pin)>>{};
 
         template<int Pin>
         struct MakeAction<Action::Read,Register::PinLocation<0,Pin>> :
