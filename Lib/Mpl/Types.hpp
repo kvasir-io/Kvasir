827946f860d5bff476ad141dc9e8ab2fcd2bda0e
Reto Schneider <code@reto-schneider.ch>
2016-12-02 19:08:38 +0100

    Revert "Remove unused define BRIGAND_NO_BOOST_SUPPORT"
    This reverts commit b67c4ce497f24d412a8fbdf293635c66a88bd4cf.

--- Lib/Mpl/Types.hpp
+++ Lib/Mpl/Types.hpp
@@ -12,6 +12,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ****************************************************************************/
 #pragma once
+#define BRIGAND_NO_BOOST_SUPPORT 1
 #include "brigand.hpp"
 
 #ifndef NDEBUG
